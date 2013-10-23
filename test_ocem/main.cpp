863c9e1cca53f41892c5d6a1b0c66f7553e99ba8
Andrea Michelotti <Andrea.Michelotti@lnf.infn.it>
2013-10-23 14:16:31 +0200

    fixed xcode project
    Change-Id: Iebe757cad925abf520b2642643c6f88a873f3d96

---
+++ test_ocem/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test_ocem
+//
+//  Created by andrea michelotti on 10/23/13.
+//  Copyright (c) 2013 andrea michelotti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
